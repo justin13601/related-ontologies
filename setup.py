d871e4382e3d1c77ed21d0ba419333b7e50d1790
Justin <justin13601@hotmail.com>
2022-07-18 23:09:23 -0400

    07/18/22 - v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='related-ontologies',
-    version='0.0.1',
+    version='0.0.2',
     description='A package to generate related clinical ontologies (LOINC, SNOMED-CT)',
     url='https://github.com/justin13601/related-ontologies',
     author='Justin Xu',
@@ -14,12 +14,4 @@ setuptools.setup(
         'pytest>=4.2.0', 'scikit-learn>=0.23.2'
     ],
     python_requires='>=3.6',
-    entry_points={
-        'console_scripts': ['related-ontologies = related-ontologies.__main__:main'],
-        'related-ontologies.__main__':
-            [
-                'apply = related-ontologies.__main__:apply',
-                'download = related-ontologies.__main__:download'
-            ]
-    },
 )
